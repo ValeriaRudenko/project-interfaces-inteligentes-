1b4b8f2ed7330a39556a7c38f16b2db2b7307b7d
Petrov Oleh <81036385+petrovoleh@users.noreply.github.com>
2023-12-28 12:11:47 +0000

    gift spawn

--- Final/Assets/Scripts/GetGifts.cs
+++ Final/Assets/Scripts/GetGifts.cs
@@ -6,7 +6,21 @@ using TMPro;
 public class GetGifts : MonoBehaviour
 {
     private int score = 0;
+    
+    public GameObject gift1;
+    public GameObject gift2;
     public TextMeshProUGUI scoreText;
+    void SpawnGift(){
+
+        Vector3 position = new Vector3(Random.Range(-8.0F, 8.0F), 1, Random.Range(-8.0F, 8.0F));
+        int gift =Random.Range(-1, 1);
+        if(gift>=0){
+            Instantiate (gift1, position, Quaternion.identity);
+        }else{
+
+            Instantiate (gift2, position, Quaternion.identity);
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Gift"))
@@ -14,6 +28,7 @@ public class GetGifts : MonoBehaviour
             Destroy(other.gameObject);
             score +=1;
             scoreText.text = Mathf.Ceil(score).ToString();
+            SpawnGift();
         }
     }
 }
