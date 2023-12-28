61ba71e5940ba14fe3f237b5016ba8592a966a5a
Petrov Oleh <81036385+petrovoleh@users.noreply.github.com>
2023-12-28 12:59:03 +0000

    gifts

--- Final/Assets/Scripts/GetGifts.cs
+++ Final/Assets/Scripts/GetGifts.cs
@@ -10,9 +10,13 @@ public class GetGifts : MonoBehaviour
     public GameObject gift1;
     public GameObject gift2;
     public TextMeshProUGUI scoreText;
-    void SpawnGift(){
 
-        Vector3 position = new Vector3(Random.Range(-8.0F, 8.0F), 1, Random.Range(-8.0F, 8.0F));
+    public int count = 50;
+    void SpawnGift(){
+        
+        float randomXOffset = (Random.Range(0, 2) == 0) ? 0.0F : 63.0F;
+Vector3 position = new Vector3(Random.Range(-9.0F, 25.0F) + randomXOffset, 2.5F, Random.Range(-68.0F, 5.0F));
+
         int gift =Random.Range(-1, 1);
         if(gift>=0){
             Instantiate (gift1, position, Quaternion.identity);
@@ -21,14 +25,16 @@ public class GetGifts : MonoBehaviour
             Instantiate (gift2, position, Quaternion.identity);
         }
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
-        if (other.CompareTag("Gift"))
+        if (other.gameObject.CompareTag("Gift"))
         {
             Destroy(other.gameObject);
             score +=1;
             scoreText.text = Mathf.Ceil(score).ToString();
+            for(int i=0; i <800; i++){
             SpawnGift();
+            }
         }
     }
 }
