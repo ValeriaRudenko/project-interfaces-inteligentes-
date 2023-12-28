6407bba160b7491d56c3f1282dc7f1ddc1992733
Petrov Oleh <81036385+petrovoleh@users.noreply.github.com>
2023-12-28 19:23:15 +0000

    scene changes

--- Final/Assets/Scripts/HitPoints.cs
+++ Final/Assets/Scripts/HitPoints.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class HitPoints : MonoBehaviour
 {
     public int hp = 100;
+    
     
     public TextMeshProUGUI scoreText;
 
@@ -14,6 +16,11 @@ public class HitPoints : MonoBehaviour
         if (other.gameObject.CompareTag("Skeleton"))
         { 
             scoreText.text = "HP: " + Mathf.Ceil(hp).ToString();          
+        }
+        if (hp <= 0){
+            SceneManager.LoadScene("Lost");
+            Cursor.lockState = CursorLockMode.None; 
+        Cursor.visible = true; 
         }
     }
 }
