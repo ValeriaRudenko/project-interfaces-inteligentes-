58f16aea0a92af4ef6ec07ed8891453bb16b7f44
Petrov Oleh <81036385+petrovoleh@users.noreply.github.com>
2023-12-28 11:56:26 +0000

    update

--- Final/Assets/Scripts/FPS.cs
+++ Final/Assets/Scripts/FPS.cs
@@ -11,6 +11,6 @@ public class FPS : MonoBehaviour
 	void Update () {
 		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
 		float fps = 1.0f / deltaTime;
-		fpsText.text = Mathf.Ceil (fps).ToString ();
+		fpsText.text = "FPS: "+Mathf.Ceil (fps).ToString ();
 	}
 }
---
+++ Final/Assets/Scripts/GetGifts.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class GetGifts : MonoBehaviour
+{
+    private int score = 0;
+    public TextMeshProUGUI scoreText;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Gift"))
+        {
+            Destroy(other.gameObject);
+            score +=1;
+            scoreText.text = Mathf.Ceil(score).ToString();
+        }
+    }
+}
